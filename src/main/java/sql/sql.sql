a7de4af5656186d830e902b192cc6151a353e941
mohammadnoghrei <noghreimohammad@gmail.com>
2024-02-07 00:24:27 +0330

    fix

---
+++ src/main/java/sql/sql.sql
@@ -0,0 +1,29 @@
+create table if not exists admin(
+    admin_id serial primary key ,
+    username varchar(255),
+    password varchar(255)
+);
+
+create table if not exists users(
+    user_id serial primary key ,
+    username varchar(255),
+    password varchar(255)
+);
+
+create table if not exists category(
+    category_id serial primary key ,
+    category_name varchar(255)
+);
+
+create table if not exists product(
+    product_id serial primary key ,
+    product_name varchar(255),
+    category_id_fk int references category(category_id)
+);
+create table if not exists cart(
+    cart_id serial primary key ,
+    product_id_fk int references product(product_id),
+    count int,
+    total_price int
+);
+select * from category where category_id =2
